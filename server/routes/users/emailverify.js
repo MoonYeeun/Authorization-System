59a6ab9a9b476219287f30a6cd2803094e1f20fc
MoonYeeun <aams82@hanmail.net>
2020-01-27 01:45:53 +0900

    modified

--- server/routes/users/emailverify.js
+++ server/routes/users/emailverify.js
@@ -17,15 +17,13 @@ router.get('/:emailCode', async (req, res) => {
     });
 
     let user_data = JSON.parse(result);
-    if(!user_data) {
-        res.redirect('http://localhost:3000/EmailVerifyFail');
-    } else {
+    if(!user_data) res.redirect('http://localhost:3000/EmailVerifyFail');
+    else {
         let selectQuery = 'SELECT COUNT(*) count FROM user WHERE user_id = ?';
         let selectResult = await db.queryParam_Arr(selectQuery, [user_data.user_id]);
         // 이미 등록된 사용자이거나 err 발생한 경우 
-        if (!selectResult || selectResult[0].count == 1) {
-            res.redirect('http://localhost:3000/EmailVerifyFail');
-        } else {
+        if (!selectResult || selectResult[0].count == 1) res.redirect('http://localhost:3000/EmailVerifyFail');
+        else {
             //users table에 새로운 user 등록
             let insertQuery = 'INSERT INTO user (user_id, user_pwd, user_name, user_salt) values ( ?, ?, ?, ?)';
             let insertResult = db.queryParam_Arr(insertQuery, [user_data.user_id, user_data.user_pwd, user_data.user_name, user_data.user_salt]);
